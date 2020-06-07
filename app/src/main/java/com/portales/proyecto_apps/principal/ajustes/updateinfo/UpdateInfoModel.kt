469f6975f04f82e55b38114b30fb9359401e2e68
Andrei-Portales <30614183+Andrei-Portales@users.noreply.github.com>
2020-06-07 11:14:26 -0600

    Settings

---
+++ app/src/main/java/com/portales/proyecto_apps/principal/ajustes/updateinfo/UpdateInfoModel.kt
@@ -0,0 +1,4 @@
+package com.portales.proyecto_apps.principal.ajustes.updateinfo
+
+data class UpdateInfoModel (var name:String = "", var birth:String ="", var height:String = "", var weight:String = "") {
+}
