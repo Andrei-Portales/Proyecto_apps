7966139bcaeccd0106d7262a0f4f5518d2d8b16d
Jose Hurtarte <JJHH121106@gmail.com>
2020-06-08 16:14:55 -0600

    RepositoryAddRoutine

---
+++ app/src/main/java/com/portales/proyecto_apps/principal/publicar/addroutine/AddRoutineRepository.kt
@@ -0,0 +1,30 @@
+package com.portales.proyecto_apps.principal.publicar.addroutine
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.firebase.firestore.FirebaseFirestore
+import com.portales.proyecto_apps.principal.publicacionmodel.RutinaModel
+
+class AddRoutineRepository {
+    private val db = FirebaseFirestore.getInstance()
+
+
+    fun putRoutine(): LiveData<List<RutinaModel>> {
+        val MutableData = MutableLiveData<List<RutinaModel>>()
+        db.collection("rutinas").get().addOnCompleteListener {
+            val list = ArrayList<RutinaModel>()
+            if (it.isSuccessful){
+                for (d in it.result!!){
+                    list.add(
+                        RutinaModel(d.get("title").toString(),
+                        d.get("description").toString(),d.get("time").toString().toFloat()
+                        , d.get("user").toString(), d.get("video").toString())
+                    )
+
+                }
+                MutableData.value = list
+            }
+        }
+        return MutableData
+    }
+}
