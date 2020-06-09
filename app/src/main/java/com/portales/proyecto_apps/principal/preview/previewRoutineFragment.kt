c89c245e3a18b8bee2eb9aee8d710a0bcc1d361d
Jose Hurtarte <JJHH121106@gmail.com>
2020-06-09 10:41:15 -0600

    Finish community

--- app/src/main/java/com/portales/proyecto_apps/principal/preview/previewRoutineFragment.kt
+++ app/src/main/java/com/portales/proyecto_apps/principal/preview/previewRoutineFragment.kt
@@ -1,5 +1,8 @@
 package com.portales.proyecto_apps.principal.preview
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -13,7 +16,9 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.portales.proyecto_apps.R
 import com.portales.proyecto_apps.databinding.FragmentPreviewRoutineBinding
+import com.portales.proyecto_apps.principal.inicio.rutinaVistaPrevia.RutinaVistaPreviaActivity
 import com.portales.proyecto_apps.principal.publicacionmodel.RutinaModel
+import kotlinx.android.synthetic.main.rutina_model_recycler_inicio.view.*
 
 /**
  * A simple [Fragment] subclass.
@@ -39,8 +44,8 @@ class previewRoutineFragment : Fragment() {
 
         val idDelObjeto = previewRoutineFragmentArgs.fromBundle(requireArguments()).selectedRoutineId
         viewModelFactory = PreviewRoutineViewModelFactory(application,idDelObjeto)
-        Toast.makeText(context, idDelObjeto, Toast.LENGTH_LONG)
-            .show()
+       // Toast.makeText(context, idDelObjeto, Toast.LENGTH_LONG)
+           // .show()
 
         viewModel = ViewModelProviders.of(this, viewModelFactory)
             .get(PreviewRoutineViewModel::class.java)
@@ -48,24 +53,53 @@ class previewRoutineFragment : Fragment() {
 
         binding.previewRoutine = viewModel
         binding.lifecycleOwner = this
-
-
+        getData()
         binding.buttonGotoAddRoutine.setOnClickListener{
             //AquiLanzo el intent
+            accion()
         }
 
          return binding.root
+    }
+    private fun accion(){
+            val intent = Intent(context, RutinaVistaPreviaActivity::class.java)
+            val element = dataList.getMap()
+            intent.putExtra("data", element)
+            (context as Activity).startActivity(intent)
+
     }
 
     private fun getData(){
         val modelLiveData: LiveData<RutinaModel> = viewModel.getData()
         modelLiveData.observe(binding.lifecycleOwner!!, object: Observer<RutinaModel>{
+            @SuppressLint("SetTextI18n")
             override fun onChanged(t: RutinaModel?) {
 
                 dataList = t!!
                 binding.routinePreviewTitle.text = dataList.title
                 binding.routineDescriptionPreview.text = dataList.description
                 binding.authorNamePreview.text = dataList.user
+
+                binding.imageRoutineIntensityPreview.setImageResource(when {
+                    dataList.time.toFloat()>=40 -> {
+                        R.drawable.routine_intensity_3
+                    }
+                    dataList.time.toFloat()>=20 -> {
+                        R.drawable.routine_intensity_2
+                    }
+
+                    else -> {
+                        R.drawable.routine_intensity_1
+                    }
+                })
+
+                var texto_ejercicio =""
+                for (ejer in dataList.exercises){
+                    texto_ejercicio += ejer.title+"\n"+ejer.description+"\n\n"
+                }
+                binding.excercisesPreview.text = texto_ejercicio
+
+                binding.routineDurationPreview.text = "Duracion: "+dataList.time.toInt()+"min"
 
 
             }
