6c629b083024dfe8fbe10bbab47b8af57208f475
Jose Hurtarte <JJHH121106@gmail.com>
2020-06-09 05:18:01 -0600

    progreso preview

---
+++ app/src/main/java/com/portales/proyecto_apps/principal/preview/previewRoutineFragment.kt
@@ -0,0 +1,17 @@
+package com.portales.proyecto_apps.principal.preview
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.portales.proyecto_apps.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [previewRoutineFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class previewRoutineFragment : Fragment() {
+}
+
